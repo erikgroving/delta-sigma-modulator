042323883b6acd765d5ff187d7c774cd49bc00c1
Erik Groving <egmeade@live.com>
2017-01-20 00:19:36 -0500

    seems alright, slight inaccuracies but only at very small values

--- dsm_tb.v
+++ dsm_tb.v
@@ -41,10 +41,10 @@ module  dsm_tb ();
 		end
 		else begin
 			if (pwm) begin
-				$fdisplay(write_file, "1");			
+				$fdisplay(write_file, "1\t%4.0f", $time);			
 			end
 			else begin
-				$fdisplay(write_file, "-1");
+				$fdisplay(write_file, "-1\t%4.0f", $time);
 			end
 			scan_file = $fscanf(data_file, "%b\n", vin); 
 			if ($feof(data_file)) begin
