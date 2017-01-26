b7ddc6b1a770404760de5f72ad56ecfed94b3d84
Erik Groving <egmeade@live.com>
2017-01-26 13:56:13 -0500

    mixer amplitude updated

--- mixer.v
+++ mixer.v
@@ -1,14 +1,16 @@
-// This doesn't need to be its own module, by the way
+// This doesn't need to be its own module
 module mixer (
 	input	[19: 0]	interp_i,
 	input	[19: 0] LO,
 	output	[19: 0] mix_o
 );
-	wire signed [39: 0] mult_res;
+
+	wire signed [24: 0] ampl;
 	
-	assign mult_res = LO * interp_i;
-	// Since ampl = -4, and gain of 0.5, we multiply by -2, 
-	// which can be done by bitshifting left once, and then
-	// negating (15 bits originally, only shift right 14)
-	assign mix_o = -(mult_res[33:14]);
+	assign ampl = 25'h028_619a;	// 0.6309573 * 0.5 = 0.3154786
+	
+	assign mix_o = ampl * $signed(interp_i) * $signed(LO);
+	
+	
+
 endmodule
