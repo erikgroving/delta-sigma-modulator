5ff6cf8fef090a1d1eff2e1de7eae2fb70cfa869
Erik Groving <egmeade@live.com>
2017-03-02 14:10:08 -0500

    Uniform random done

---
+++ lfsr_tb.v
@@ -0,0 +1,41 @@
+module lfsr_tb (
+
+);
+
+	reg					clock;
+	reg 				reset;
+	wire signed [19: 0]	lfsr;
+	
+	integer lfsr_out;
+	
+	
+	lfsr lfsr_i(
+		.clock(clock),
+		.reset(reset),
+		.lfsr_o(lfsr)
+	);
+	
+	always begin
+		#5;
+		clock = ~clock;
+	end
+	
+	initial begin
+		clock = 1'b0;
+		reset = 1'b1;
+		lfsr_out = $fopen("../lfsr_out.txt", "w");
+		@(posedge clock);
+		#1;
+		reset = 1'b0;
+		#5000000;
+		$fclose(lfsr_out);
+		$finish;
+	end
+		
+	always @(negedge clock) begin
+		if (!reset) begin
+			$fdisplay(lfsr_out, "%d", lfsr);
+		end
+	end
+		
+endmodule
