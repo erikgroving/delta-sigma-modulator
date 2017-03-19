a93bdf018511625a3c34ebd02c25a8fade1d5f42
Erik Groving <egmeade@live.com>
2017-03-19 17:02:45 +0100

    test new dsm

--- mixer.v
+++ mixer.v
@@ -9,21 +9,21 @@
 // ampl multiply a bitshift-sum operation.
 `include "parameters.vh"
 module mixer (
-	input	[`T_BITS - 1: 0]	interp_i,
+	input	[14: 0]	interp_i,
 	input	[1: 0] 				LO,
-	output	[`T_BITS - 1: 0]	mix_o
+	output	[14: 0]	mix_o
 );
 
-	wire signed [`T_BITS - 1: 0] 		ampl;
-	wire signed [`T_BITS - 1: 0] 		inter_res;
-	wire signed	[2 * `T_BITS - 1: 0] 	mix_res;
+	wire signed [14: 0] ampl;
+	wire signed [14: 0] inter_res;
+	wire signed	[29: 0] mix_res;
 	
 	assign ampl = 15'h2861;	// 0.6309573 * 0.5 = 0.3154786 (15 fractional bits)
 	
 	assign inter_res	= 	LO[1] ? -interp_i :
-							LO[0] ? interp_i : `T_BITS'b0;
+							LO[0] ? interp_i : 15'b0;
 							
 	assign mix_res		= inter_res * ampl;
-	assign mix_o		= mix_res[`T_BITS + 14: 15];
+	assign mix_o		= mix_res[29: 15];
 
 endmodule
